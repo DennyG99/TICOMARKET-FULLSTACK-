74b33ef36772da0cb6d753ea60ea92dc99340ec1
QuiqueCode <dylanbusisness@gmail.com>
2023-11-16 17:36:02 -0600

    Solucion

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from 'react'
 import './App.css'
-import { GestionSuperAdministrador } from './components/GestionAdministrador/gestionSuperAdmin'
-import { GestionModerador } from './components/GestionAdministrador/gestionModerador'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   <GestionModerador></GestionModerador>
     <h1>Content</h1>
     </>
   )
