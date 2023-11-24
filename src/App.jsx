54e202c46a4e14baeca9747b41f9540b3cb3932e
Kendel~ <kendell21am@gmail.com>
2023-11-24 11:30:38 -0600

    24-11 11:30

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import { GestionModerador } from './components/GestionAdministrador/gestionModerador'
 import { GestionSuperAdministrador } from './components/GestionAdministrador/gestionSuperAdmin'
-<<<<<<< HEAD
-
+import ApartadoEstadosV2 from './components/GestionarEstados/ApartadoEstadosV2'
 import { CardPoliticas } from './components/GestionAdministrador/Politicas/CardPoliticas'
 import { CardPlanes } from './components/GestionAdministrador/Planes/CardPlanes'
-//import { CardPlanes } from './components/GestionAdministrador/Planes/CardPlanes'
-
-=======
-import ApartadoEstadosV2 from './components/GestionarEstados/ApartadoEstadosV2'
->>>>>>> 4755e744c1b3676e76f2fa1b0dbaa196748cb2d6
 function App() {
   const [count, setCount] = useState(0)
 
