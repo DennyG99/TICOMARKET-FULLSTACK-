4755e744c1b3676e76f2fa1b0dbaa196748cb2d6
TristanTico <125488520+TristanTico@users.noreply.github.com>
2023-11-24 10:24:00 +0100

    Apartado de gestion de estados
    Se instalo sweetalert, en el index html se agrego el cdn de los iconos. Y se agrego el crud de gestion de estados

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { GestionModerador } from './components/GestionAdministrador/gestionModerador'
 import { GestionSuperAdministrador } from './components/GestionAdministrador/gestionSuperAdmin'
+import ApartadoEstadosV2 from './components/GestionarEstados/ApartadoEstadosV2'
 function App() {
   const [count, setCount] = useState(0)
 
